f208333ccf5bafd32969b2fd8bacc72cdb033f82
Jack Gronenthal <jlg6@illinois.edu>
2023-02-22 12:55:32 -0800

    Initial Commit

---
+++ flyt/flytApp.swift
@@ -0,0 +1,17 @@
+//
+//  flytApp.swift
+//  flyt
+//
+//  Created by Jack Gronenthal on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct flytApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
